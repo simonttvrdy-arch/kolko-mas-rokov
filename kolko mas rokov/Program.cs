543e0903c61fdf9f61223b6d7fbb5f24f38b2430
Tvrdys26 <tadeasimon@gmail.com>
2026-09-16 14:10:39 +0200

    Add project files.

---
+++ kolko mas rokov/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Zadaj rok narodenia: ");
+string PrveCislo = Console.ReadLine();
+int číslo2 = int.Parse(PrveCislo);
+
+int tentorok = DateTime.Now.Year;
+
+int vysledok = tentorok - číslo2;
+Console.WriteLine("Tvoj vek: " + vysledok);
+
+
+
+
+
+
